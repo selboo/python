b3c717e1dc70de277757077fee5ebe01b92b2f6d
Selboo <xuguocan@gmail.com>
2013-03-16 16:55:53 +0800

    过滤转移

--- day01/list.py
+++ day01/list.py
@@ -9,3 +9,21 @@ for i in range(0,len(a)):
 
 print n
 print b
+print "========================="
+a1 = "\t\t\hello\n.py\n\n\n"
+a2 = r"\t\t\hello\n.py\n\n\n"
+a3 = "\hello\n.py"
+a4 = r"\hello\n.py"
+
+print a1
+print a2
+print a3
+print a4
+print "========================="
+print a1.strip()
+print a2.strip()
+print a3.strip()
+print a4.strip()
+
+
+
